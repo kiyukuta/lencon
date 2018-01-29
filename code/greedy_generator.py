16c65471b30436e4e96049bbdccf6c49c9b1f1f4
Yuta Kikuchi <yut.kkc@gmail.com>
2018-01-29 11:44:14 +0900

    fix greedy decoder

--- code/greedy_generator.py
+++ code/greedy_generator.py
@@ -4,6 +4,7 @@ class GreedyGenerator:
     def __init__(self, model, xvocab, yvocab, beam_width=10):
         self.model = model
         self.xvocab = xvocab
+        self.yvocab = yvocab
 
     def generate(self, source, length, byte, allow_unk=False):
         seq, score = self.model(source, None, length, train=False)
